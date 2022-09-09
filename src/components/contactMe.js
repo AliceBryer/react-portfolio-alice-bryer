5a9b193dcf10e1156caa783b26f448f123e7ac9e
Alice Bryer <alicebryer123@gmail.com>
2022-09-09 15:24:11 +0100

    added basic styling to form

--- src/components/contactMe.js
+++ src/components/contactMe.js
@@ -9,35 +9,46 @@ import AccountCircle from "@mui/icons-material/AccountCircle";
 
 const contactMe = () => {
   return (
-    <Box sx={{ "& > :not(style)": { m: 1 } }}>
-      <FormControl variant="standard">
-        <InputLabel htmlFor="input-with-icon-adornment">Name</InputLabel>
-        <Input
-          id="input-with-icon-adornment"
-          startAdornment={
-            <InputAdornment position="start">
-              <AccountCircle />
-            </InputAdornment>
-          }
+    <div>
+      <h2 className="contact-me-title">
+        {" "}
+        Want to contact me? Fill in the form below or you can reach me
+        alicebryer123@gmail.com or my{" "}
+        <a href="https://www.linkedin.com/in/alicebryer123/" target="_blank">
+          {" "}
+          linkedin
+        </a>
+      </h2>
+      <Box className="form" sx={{ "& > :not(style)": { m: 1 } }}>
+        <FormControl variant="standard">
+          <InputLabel htmlFor="input-with-icon-adornment">Name</InputLabel>
+          <Input
+            id="input-with-icon-adornment"
+            startAdornment={
+              <InputAdornment position="start">
+                <AccountCircle />
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+        <TextField
+          id="input-with-icon-textfield"
+          label="Email"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <AccountCircle />
+              </InputAdornment>
+            ),
+          }}
+          variant="standard"
         />
-      </FormControl>
-      <TextField
-        id="input-with-icon-textfield"
-        label="Email"
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <AccountCircle />
-            </InputAdornment>
-          ),
-        }}
-        variant="standard"
-      />
-      <Box sx={{ display: "flex", alignItems: "flex-end" }}>
-        <AccountCircle sx={{ color: "action.active", mr: 1, my: 0.5 }} />
-        <TextField id="input-with-sx" label="Message" variant="standard" />
+        <Box sx={{ display: "flex", alignItems: "flex-end" }}>
+          <AccountCircle sx={{ color: "action.active", mr: 1, my: 0.5 }} />
+          <TextField id="input-with-sx" label="Message" variant="standard" />
+        </Box>
       </Box>
-    </Box>
+    </div>
   );
 };
 
