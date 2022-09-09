c3eca956f9a46b4f76b628c77ebf1adbc6ad2b95
Alice Bryer <alicebryer123@gmail.com>
2022-09-09 14:41:53 +0100

    added the about me images & text

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,14 +3,46 @@ import * as React from "react";
 const aboutMe = () => {
   return (
     <div>
-      <h2>About ME</h2>
+      <h2>About Me</h2>
       <div>
-        <image></image>
-        <image></image>
-        <image></image>
+        <img
+          src="https://scontent.fbhx2-1.fna.fbcdn.net/v/t39.30808-6/305090991_5595242633872105_8560142736519921432_n.jpg?stp=cp6_dst-jpg_fr_q79&_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=zQ8HZrJ5Z90AX-PlVd8&tn=-XsPep2T47EFpemw&_nc_ht=scontent.fbhx2-1.fna&oh=00_AT_XVJsQJuU68Ole0vymoy3qWTQakI0N3q9Y4HxfmYpC8w&oe=6320847D"
+          alt="me and my dog up the great orme, Wales"
+          width="220"
+          height="240"
+        ></img>
+        <img
+          src="https://scontent.fbhx2-1.fna.fbcdn.net/v/t1.6435-9/71037024_10217366230680176_6485800800537804800_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=XG6HKSrSnNEAX-mUxIY&_nc_ht=scontent.fbhx2-1.fna&oh=00_AT-6dhElqrEj8I4-KxZpI-KSdoZhilIy3p-HTFNtFo5CTQ&oe=633F9255"
+          alt="me and my boyfriend"
+          width="220"
+          height="240"
+        ></img>
+        <img
+          src="https://scontent.fbhx2-1.fna.fbcdn.net/v/t1.6435-9/49949776_10156211693649067_8519616476896821248_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=1czFE6zOJQ8AX8pvGF6&_nc_ht=scontent.fbhx2-1.fna&oh=00_AT86vj3BLJJtiHRWWBMi20c0d7Q2zeKA8EOgiQg14RlIpw&oe=633EE3CA"
+          alt="me and my family"
+          width="220"
+          height="240"
+        ></img>
+        <img
+          src="https://scontent.fbhx2-1.fna.fbcdn.net/v/t39.30808-6/241987802_4512699005459812_6356588029784817189_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=174925&_nc_ohc=FHKT3t9DYeUAX8s4ReP&_nc_ht=scontent.fbhx2-1.fna&oh=00_AT9FFrQOllvzyAlcC-Ng_7alKZdJX2gYdtRzTFnyDMGhkQ&oe=63203DFD"
+          alt="my, my partner and our nephew"
+          width="220"
+          height="240"
+        ></img>
+        <img
+          src="https://scontent.fbhx2-1.fna.fbcdn.net/v/t39.30808-6/279213824_5250894018306970_1509991312949307998_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=sARdg2PhRA4AX_hMzIe&_nc_ht=scontent.fbhx2-1.fna&oh=00_AT-fQoX4NSX2XnSmLbOuHYOr30eIGDua4B344FqrzRvboQ&oe=631F5A0C"
+          alt="my dog, Murphy"
+          width="220"
+          height="240"
+        ></img>
       </div>
       <div className="bio">
-        <p> My name is Alice , I'm 28 etc, I live in the West Midlands</p>
+        <p>
+          {" "}
+          Welcome to my Bio! I'm Alice and I live in Staffordshire, West
+          Midlands. I love spending time with my family, my dog, my partner and
+          my Nephew (All pictured above) .
+        </p>
       </div>
     </div>
   );
