670add752267a9785af2af5b600cac8951234079
Alice Bryer <alicebryer123@gmail.com>
2022-09-09 15:47:44 +0100

    updated skills

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,7 +2,7 @@ import * as React from "react";
 
 const resume = () => {
   return (
-    <div>
+    <div className="container">
       <div className="Resume">
         <h1>Resume</h1>
         <h2>
@@ -11,13 +11,28 @@ const resume = () => {
             download="Alice Bryer-CV"
             target="_blank"
           >
-            Clickable download link
+            Download my CV!
           </a>
         </h2>
       </div>
       <div className="Skills">
-        <h1>SKILLS </h1>
-        <p>HTML, CSS, JS, NODE</p>
+        <h1>Skills and Technologies</h1>
+        <ul>
+          <li>HTML, CSS and JS</li>
+          <li>Server-side and third party api's</li>
+          <li>Node.js</li>
+          <li>OOP</li>
+          <li>Express</li>
+          <li>SQL</li>
+          <li>ORM</li>
+          <li>MVC</li>
+          <li>CS</li>
+          <li>MongoDB</li>
+          <li>PWA</li>
+          <li>React</li>
+          <li>MERN</li>
+          <li>State</li>
+        </ul>
       </div>
     </div>
   );
