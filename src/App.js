02fe7bca5755ba0336eb7e1c5c9c71b264ac0b6e
Alice Bryer <alicebryer123@gmail.com>
2022-09-09 10:48:49 +0100

    set up app.js file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from "./logo.svg";
+import React {useEffect} from "react";
+import { BrowserRouter as Router, Routes, Route} from"react-router-dom";
+import Wrapper from "./components/Wrapper";
 import "./App.css";
 
+import Header from "";
+import AboutMe from "";
+import Portfolio from "";
+import ContactMe from "";
+import Resume from "";
+import Footer from "";
+
 function App() {
+  useEffect(() => {
+    document.title = "Alice Bryer - React Portfolio";
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React HEY
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Header />
+    <Wrapper>
+      <Routes>
+        <Route path="/" element={<AboutMe />} />
+        <Route path="/Portfolio" element={<Portfolio />} />
+        <Route path="/ContactMe" element={<ContactMe /> }/>       
+        <Route path="/Resume" element={<Resume />} />
+        </Routes>
+    </Wrapper>
+    <Footer />
+  </Router>
+    
   );
 }
 
